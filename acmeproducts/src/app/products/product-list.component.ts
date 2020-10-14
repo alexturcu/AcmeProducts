584c082ce44091ea2e7cda646a5c19da0d94ed22
Alexandru Turcu <alexandru.turcu@SVROLP01666.ro.softvision.global>
2020-10-14 17:41:03 +0300

    Used OnInit life-cycle hook

--- acmeproducts/src/app/products/product-list.component.ts
+++ acmeproducts/src/app/products/product-list.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -36,5 +36,9 @@ export class ProductListComponent {
 
     toggleImage(): void {
       this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+      console.log('In OnInit');
     }
 }
