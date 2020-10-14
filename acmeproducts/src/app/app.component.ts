f92a84370268a988210b6b94d524af889514cb7a
Alexandru Turcu <alexandru.turcu@SVROLP01666.ro.softvision.global>
2020-10-14 12:52:13 +0300

    Added AppComponent

--- acmeproducts/src/app/app.component.ts
+++ acmeproducts/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'pm-root',
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'acmeproducts';
+  pageTitle: string = 'Acme Product Management';
 }
