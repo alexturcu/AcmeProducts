9283a3b7f58713981945858e63cf77224de66761
Alexandru Turcu <alexandru.turcu@SVROLP01666>
2020-10-15 16:28:39 +0300

    Added service class

--- acmeproducts/src/app/products/product-list.component.ts
+++ acmeproducts/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -22,49 +23,28 @@ export class ProductListComponent implements OnInit {
     }
     
     filteredProducts: IProduct[];
-    products: any[]= [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-      this.filteredProducts = this.products;
-      this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
+      
     }
-
+    
     onRatingClicked(message: string): void {
       this.pageTitle = 'Product List: ' + message;
     }
-
+    
     performFilter(filterBy: string): IProduct[] {
       filterBy = filterBy.toLocaleLowerCase();
       return this.products.filter((product: IProduct) =>
-             product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
-
+    
     toggleImage(): void {
       this.showImage = !this.showImage;
     }
-
+    
     ngOnInit(): void {
-      console.log('In OnInit');
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
     }
 }
