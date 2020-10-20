2560a9a0b4016b33a4905bb65906cad344020094
Alexandru Turcu <alexandru.turcu@SVROLP01666>
2020-10-20 20:19:57 +0300

    Added routes

--- acmeproducts/src/app/products/product-detail.component.ts
+++ acmeproducts/src/app/products/product-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  product: IProduct | undefined;
 
   constructor() { }
 
